f7671d68a6b2fc255423263d42f24a061d0d32f7
Livitan <>
2021-01-17 03:05:01 +0200

    Guardian Renewal

--- Enums/Spells/Druid.lua
+++ Enums/Spells/Druid.lua
@@ -68,7 +68,8 @@ Spells.DRUID = {
 		   Swipe = {213764},
 		   MightyBash = {5211},
 		   Thrash = {106832},
-           IncarnationGuardianOfUrsoc = {102558}
+           IncarnationGuardianOfUrsoc = {102558},
+           Renewal = {108238}
         },
         Buffs = {
             EclipseLunar = {48518},
@@ -88,7 +89,8 @@ Spells.DRUID = {
 			-- WarMachine = 22632, -- 262231
 			BloodTalons = 21649,
 			BrutalSlash = 21711,
-            IncarnationGuardianOfUrsoc = 22388
+            IncarnationGuardianOfUrsoc = 22388,
+            Renewal = 18570
 		},
 
     },
